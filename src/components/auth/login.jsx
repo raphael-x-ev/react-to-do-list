d4863432d31d7e0837966d34b876e1b9db3c3f30
rap.ev <raphaelmoa12@gmail.com>
2021-08-04 07:44:37 +0800

    Added validations to form

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,31 +1,125 @@
 import React, { Component } from "react";
+import { FormErrors } from "./FormErrors";
 import "./login.css";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      formErrors: { username: "", password: "" },
+      usernameValid: false,
+      passwordValid: false,
+      authorized: false,
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value }, () => {
+      this.validateField(name, value);
+    });
+  };
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let usernameValid = this.state.usernameValid;
+    let passwordValid = this.state.passwordValid;
+
+    switch (fieldName) {
+      case "username":
+        usernameValid = value.match(/^[a-zA-Z0-9_]{3,10}$/);
+        fieldValidationErrors.username = usernameValid ? "" : "Invalid";
+        break;
+      case "password":
+        passwordValid = value.match(/^[^&^$#]+$/);
+        fieldValidationErrors.password = passwordValid ? "" : "Invalid";
+        break;
+      default:
+        break;
+    }
+    this.setState({
+      formErrors: fieldValidationErrors,
+      usernameValid: usernameValid,
+      passwordValid: passwordValid,
+    });
+  }
+
+  authorize(e) {
+    let fieldValidationErrors = this.state.formErrors;
+    const username = e.target.querySelector('input[name="username"]').value;
+    const password = e.target.querySelector('input[type="password"]').value;
+
+    const auth = password === "123456" && username === "myaccount";
+    fieldValidationErrors.credentials = auth ? "" : "Invalid";
+    console.log(auth);
+    e.preventDefault();
+    this.setState({
+      authorized: auth,
+    });
+  }
+
+  // validateForm() {
+  //   this.setState({
+  //     formValid: this.state.usernameValid && this.state.passwordValid,
+  //   });
+  // }
+
+  errorClass(error) {
+    return error.length === 0 ? "" : "has-error";
+  }
+
   render() {
     return (
       <div className="auth-wrapper">
         <div className="auth-inner">
-          <form>
+          <form action="#" onSubmit={this.authorize}>
             <h3>Sign In</h3>
-
             <div className="form-group">
-              <input
-                type="username"
-                className="form-control"
-                placeholder="Username"
-              />
+              <div
+                className={`form-group ${this.errorClass(
+                  this.state.formErrors.username
+                )}`}
+              >
+                <input
+                  name="username"
+                  type="text"
+                  className="form-control"
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.handleUserInput}
+                />
+              </div>
             </div>
-
             <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
+              <div
+                className={`form-group ${this.errorClass(
+                  this.state.formErrors.password
+                )}`}
+              >
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleUserInput}
+                />
+              </div>
             </div>
-
-            <button type="submit" className="btn btn-primary btn-block">
+            <div className="panel panel-default">
+              <FormErrors formErrors={this.state.formErrors} />
+            </div>
+            <button
+              name="submit"
+              type="submit"
+              className="btn btn-primary btn-block"
+              onClick={this.handleUserInput}
+              // disabled={!this.state.formValid}
+            >
               Login
             </button>
           </form>
